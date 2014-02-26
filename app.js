3c531b9ea022ad10bd320f3223b964c6e34be86a
Ale Prieto <aleprieto@gmail.com>
2014-02-26 17:31:01 -0300

    Se cambia a MongoDB, se configuran los modelos de Cuenta y Registro, se completa MVC.

--- app.js
+++ app.js
@@ -5,15 +5,38 @@
 
 var express = require('express');
 var routes = require('./routes');
+var cuentas = require('./routes/cuentas');
 var registros = require('./routes/registros');
 var user = require('./routes/user');
 var http = require('http');
+// var mysql = require('mysql');
 var path = require('path');
+
+// Variables
+var cache = {};
+var websockets = require('./libs/websocket');
 
 var app = express();
 
+// Mysql
+// var connection = mysql.createConnection({
+// 	host: 'localhost',
+// 	user: 'root',
+// 	password: 'ale'
+// })
+
+// connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+// 	if(err) throw err;
+
+// 	console.log('La solucion es: ', rows[0].solution);
+// });
+
+// connection.end();
+
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
@@ -31,8 +54,12 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/registros', registros.registrar);
+app.post('/registros', registros.add);
+app.get('/cuentas', cuentas.find);
+app.post('/cuentas', cuentas.add);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
+  websockets(this, cache);
 });
--- routes/registros.js
+++ routes/registros.js
@@ -3,6 +3,19 @@
  * GET home page.
  */
 
+var Registros = require('../models/registro');
+
 exports.registrar = function(req, res) {
   res.render('registrar', { title: 'FiNode' });
+};
+
+
+exports.add = function(req, res) {
+	registro = req.body
+	console.log(registro);
+	Registros.add(registro);
+	res.send();
+	// Registros.find(function (error, registros) {
+	// 	res.render('registros/index', {title: 'Listado', registros: registros});
+	// })
 };
