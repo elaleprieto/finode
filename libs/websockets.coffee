7cbd622379ee9fe9f579e54c034c9dbfec75bb9f
Ale Prieto <aleprieto@gmail.com>
2014-02-27 10:39:39 -0300

    Se cambia websocket.js a websocket.coffee

---
+++ libs/websockets.coffee
@@ -0,0 +1,50 @@
+module.exports = (servidor, cache, Registros) ->
+	
+	Cuentas = require('../models/cuenta')
+	Registros = require('../models/registro')
+	sio = require('socket.io')
+	ws = sio.listen(servidor)
+
+	ws.disable('log')
+	# websocket, htmlfile, xhr-polling, jsonp-polling
+	
+	ws.on 'connection', (socket) ->
+		socket.emit 'ready',{title: 'Ready'}
+
+		socket.on 'imagen', (imagen) ->
+			cache[imagen.id] = imagen.data
+			socket.broadcast.emit('imagen', imagen)
+		
+		socket.on 'color', (color) ->
+			# cache[imagen.id] = imagen.data
+			console.log('Color: ', color)
+			socket.broadcast.emit('color', color)
+
+		 # socket.on 'colorChanged', (element, rgb) ->
+			 # console.log('Elemento: ' + element, ', Color recibido: ' + rgb)
+			 # socket.broadcast.emit('colorChanged', {element: element, rgb: rgb})
+		socket.on 'colorChanged', (data) ->
+			socket.broadcast.emit('colorChanged', data)
+
+			Registros.edit({id: data.element
+					, intensidadAzul: data.rgb.b
+					, intensidadRojo: data.rgb.r
+					, intensidadVerde: data.rgb.g})
+
+		socket.on 'cambiarColor', (registro) ->
+			socket.broadcast.emit('cambiarColor', registro)
+
+			Registros.edit({id: registro._id
+					, intensidadAzul: registro.intensidadAzul
+					, intensidadRojo: registro.intensidadRojo
+					, intensidadVerde: registro.intensidadVerde})
+
+		socket.on 'add', (registro) ->
+			socket.broadcast.emit('registroAdded', registro)
+
+			Registros.add(registro)
+
+		socket.on 'cuentaAdd', (cuenta) ->
+					socket.broadcast.emit('cuentaAdded', cuenta)
+					Cuentas.add(cuenta)
+
---
+++ libs/websockets.js
@@ -0,0 +1,50 @@
+(function() {
+  module.exports = function(servidor, cache, Registros) {
+    var Cuentas, sio, ws;
+    Cuentas = require('../models/cuenta');
+    Registros = require('../models/registro');
+    sio = require('socket.io');
+    ws = sio.listen(servidor);
+    ws.disable('log');
+    return ws.on('connection', function(socket) {
+      socket.emit('ready', {
+        title: 'Ready'
+      });
+      socket.on('imagen', function(imagen) {
+        cache[imagen.id] = imagen.data;
+        return socket.broadcast.emit('imagen', imagen);
+      });
+      socket.on('color', function(color) {
+        console.log('Color: ', color);
+        return socket.broadcast.emit('color', color);
+      });
+      socket.on('colorChanged', function(data) {
+        socket.broadcast.emit('colorChanged', data);
+        return Registros.edit({
+          id: data.element,
+          intensidadAzul: data.rgb.b,
+          intensidadRojo: data.rgb.r,
+          intensidadVerde: data.rgb.g
+        });
+      });
+      socket.on('cambiarColor', function(registro) {
+        socket.broadcast.emit('cambiarColor', registro);
+        return Registros.edit({
+          id: registro._id,
+          intensidadAzul: registro.intensidadAzul,
+          intensidadRojo: registro.intensidadRojo,
+          intensidadVerde: registro.intensidadVerde
+        });
+      });
+      socket.on('add', function(registro) {
+        socket.broadcast.emit('registroAdded', registro);
+        return Registros.add(registro);
+      });
+      return socket.on('cuentaAdd', function(cuenta) {
+        socket.broadcast.emit('cuentaAdded', cuenta);
+        return Cuentas.add(cuenta);
+      });
+    });
+  };
+
+}).call(this);
